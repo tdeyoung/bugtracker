60e0c64f05c420d571b4091fcba3edc31ab69e8f
Tim DeYoung <tim.deyoung@yahoo.com>
2016-02-25 17:42:08 -0500

    Added root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :bugs
+  root 'bugs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
