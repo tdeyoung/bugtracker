46bd208b898559e1862966e0e74cad4930f0c86d
Tim DeYoung <tim.deyoung@yahoo.com>
2016-04-06 12:32:04 -0400

    post demo 6 commit, includes ajax and cancel btn

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,8 +1,15 @@
 class BugsController < ApplicationController
+  respond_to :html, :js
+
   before_action :set_issue_types
   before_action :set_priorities
   before_action :set_statuses
   before_action :set_bug, only: [:show, :edit, :update, :destroy]
+  before_action :all_bugs, only: [:index, :create, :destroy, :update]
+
+  def all_bugs
+    @bugs = Bug.all
+  end
 
   # GET /bugs
   # GET /bugs.json
@@ -26,45 +33,94 @@ class BugsController < ApplicationController
 
   # POST /bugs
   # POST /bugs.json
-  def create
-    @bug = Bug.new(bug_params)
-
-    respond_to do |format|
-      if @bug.save
-        format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
-        format.json { render :show, status: :created, location: @bug }
-        user = User.find(bug_params[:user_id])
-        @bug.build_user(:id => user.id)
-      else
-        format.html { render :new }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
+   def create
+     if params[:cancel] == "Cancel"
+         respond_to do |format|
+             format.js { render action: 'cancel'}
+         end
+     else
+         @bug = Bug.new(bug_params)
+         user = User.find(bug_params[:user_id])
+         @bug.build_user(:id  => user.id)
+         if @bug.save
+             @current_bug = @bug
+         else
+             respond_to do |format|
+                 format.js { render action: 'new' }
+              end 
+          end
+      end 
     end
-  end
+  # def create
+  #   @bug = Bug.new(bug_params)
+  #   user = User.find(bug_params[:user_id])
+  #   @bug.build_user(:id => user.id)
+  #   @bug.save!
+  #   @current_bug = @bug
+  #   @bugs = Bug.all
+  # end
+
+  # def create
+  #   @bug = Bug.new(bug_params)
+
+  #   respond_to do |format|
+  #     if @bug.save
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
+  #       format.json { render :show, status: :created, location: @bug }
+  #       user = User.find(bug_params[:user_id])
+  #       @bug.build_user(:id => user.id)
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /bugs/1
   # PATCH/PUT /bugs/1.json
-  def update
-    respond_to do |format|
-      if @bug.update(bug_params)
-        format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bug }
-      else
-        format.html { render :edit }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
+def update
+    if params[:cancel] == "Cancel"
+        respond_to do |format|
+          format.js { render action: 'cancel'}
+        end 
+    else
+        if !@bug.update(bug_params)
+            respond_to do |format|
+                format.js {render action: 'edit' }
+            end
+        end 
     end
-  end
+end
+
+  # def update
+  #   @bug.update!(bug_params)
+  # end
+
+  # def update
+  #   respond_to do |format|
+  #     if @bug.update(bug_params)
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @bug }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /bugs/1
   # DELETE /bugs/1.json
   def destroy
-    @bug.destroy
-    respond_to do |format|
-      format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @bug.destroy!
   end
+
+  # def destroy
+  #   @bug.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,7 @@
+class HomepageController < ApplicationController
+  def index
+  end
+
+  def goodbye
+  end
+end
